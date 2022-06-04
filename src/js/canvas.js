008cf5f9dd73fd6acc3b66a3eaf64a70110da836
Cam <nghiem9155@gmail.com>
2022-06-04 22:43:05 +0700

    Image platform

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,73 +1,190 @@
-import utils from './utils'
+import platform from '../img/platform.png'
 
+console.log(platform)
 const canvas = document.querySelector('canvas')
+
 const c = canvas.getContext('2d')
 
-canvas.width = innerWidth
-canvas.height = innerHeight
+canvas.width = 1024
+canvas.height = 576
 
-const mouse = {
-  x: innerWidth / 2,
-  y: innerHeight / 2
+const gravity = 0.5
+class Player {
+    constructor(){
+        this.position = {
+            x: 100,
+            y: 100
+        }
+
+        this.velocity = {
+            x: 0,
+            y: 1
+        }
+
+        this.width = 30
+        this.height = 30
+    }
+    draw() {
+        c.fillStyle = 'blue'
+        c.fillRect(this.position.x, this.
+            position.y, this.width, this.height
+            )
+    }
+
+    update() {
+        
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+
+        if(this.position.y + this.height + this.velocity.y <= canvas.height)
+        this.velocity.y += gravity
+        else 
+        this.velocity.y = 0
+    }
 }
 
-const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
+class Platform {
+    constructor({ x, y, image }) {
+        this.position = {
+            x,
+            y
+        }
 
-// Event Listeners
-addEventListener('mousemove', (event) => {
-  mouse.x = event.clientX
-  mouse.y = event.clientY
-})
+        this.image = image
+        this.width = image.width
+        this.height = image.height
+    }
 
-addEventListener('resize', () => {
-  canvas.width = innerWidth
-  canvas.height = innerHeight
-
-  init()
-})
-
-// Objects
-class Object {
-  constructor(x, y, radius, color) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
-  }
-
-  draw() {
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
-    c.closePath()
-  }
-
-  update() {
-    this.draw()
-  }
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y)
+    }
 }
 
-// Implementation
-let objects
-function init() {
-  objects = []
+const image = new Image()
+image.src = platform
 
-  for (let i = 0; i < 400; i++) {
-    // objects.push()
-  }
+console.log(image)
+
+const player = new Player()
+const platforms = [
+  new Platform({
+    x: -1,
+    y: 470,
+    image
+  }), 
+  new Platform({ x: image.width - 2, y: 470, image }
+    )
+  ]
+
+const keys = {
+    right: {
+        pressed: false
+    },
+    left: {
+        pressed: false
+    }
 }
 
-// Animation Loop
-function animate() {
-  requestAnimationFrame(animate)
-  c.clearRect(0, 0, canvas.width, canvas.height)
+let scroll0ffset = 0
 
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
-  // objects.forEach(object => {
-  //  object.update()
-  // })
-}
+    function animate() {
+        requestAnimationFrame(animate)
+        c.fillStyle = 'white'
+        c.fillRect(0, 0, canvas.width, canvas.height)
+        
+        platforms.forEach((platform) => {
+            platform.draw()
+        })
+        player.update()
+        
 
-init()
-animate()
+        if (keys.right.pressed && player.position.x < 400) {
+            player.velocity.x = 5
+        } else if (keys.left.pressed && player.position.x > 100) {
+            player.velocity.x = -5
+        } else { 
+            player.velocity.x = 0
+
+            if(keys.right.pressed) {
+                scroll0ffset += 5
+                platforms.forEach(platform => {
+                    platform.position.x -= 5;
+                })
+            } else if (keys.left. pressed) {
+                scroll0ffset -= 5
+
+                platforms.forEach(platform => {
+                    platform.position.x += 5;
+                })
+            }
+        }
+
+        console.log(scroll0ffset)
+        // platform collision detection
+        platforms.forEach(platform => {
+        if (
+            player.position.y + player.height <= platform.position.y 
+            && player.position.y + player.height + player.velocity.y 
+            >= platform.position.y && player.position.x + player.width 
+            >= platform.position.x && player.position.x 
+            <= platform.position.x + platform.width
+            ) {
+            player.velocity.y = 0
+        }
+        })
+
+        if(scroll0ffset > 2000) {
+            console.log('You win')
+
+        }
+        
+    }
+
+    animate()
+
+    addEventListener('keydown', ({ keyCode }) => {
+        console.log( keyCode )
+        switch (keyCode){
+            case 65:
+                console.log('left')
+                keys.left.pressed = true
+                break
+            case 83:
+                console.log('down')
+                break
+            case 68:
+                console.log('right')
+                keys.right.pressed = true
+                break
+            case 87:
+                console.log('up')
+                player.velocity.y -= 20
+                break
+        }
+        
+        console.log(keys.right.pressed)
+    })
+
+    
+
+    addEventListener('keyup', ({ keyCode }) => {
+        console.log( keyCode )
+        switch (keyCode){
+            case 65:
+                console.log('left')
+                keys.left.pressed = false
+                break
+            case 83:
+                console.log('down')
+                break
+            case 68:
+                console.log('right')
+                keys.right.pressed = false
+                break
+            case 87:
+                console.log('up')
+                break
+        }
+    })
+    
